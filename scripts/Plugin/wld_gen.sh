9d82d7a021ce5733220b555b3e7d73879e066a5a
sahausy10 <mohammed.siali@softathome.com>
2022-06-23 17:26:48 +0200

    Issue: SSW-5203 don't fail when /etc/environment doesn't exist
    Problem: pwhm fail to load
    
    Cause: on some situation/boards /etc/environment can not be used
    
    Solution: don't fail on non existing file
    GitOrigin-RevId: cf535d5c1adc4661cf3a954cb519b534b7ec474e

--- scripts/Plugin/wld_gen.sh
+++ scripts/Plugin/wld_gen.sh
@@ -3,16 +3,19 @@
 get_base_wan_address()
 {
     # Define WAN_ADDR env var needed by pwhm to initialize wifi devices
-    if [ -z "$WAN_ADDR" ]; then
-        export WAN_ADDR=$(cat /sys/class/net/br-lan/address 2> /dev/null)
+    if [ -e "/etc/environment" ]; then
+        set -a
+        . /etc/environment 2> /dev/null
+        if [ -n "$WAN_ADDR" ]; then
+            exit
+        fi
     fi
+    export WAN_ADDR=$(cat /sys/class/net/br-lan/address 2> /dev/null)
 }
 
 case $1 in
     start|boot)
         export LD_LIBRARY_PATH=/usr/lib/amx/wld
-        set -a
-        . /etc/environment
         get_base_wan_address
         mkdir -p /var/lib/wld
         amxrt -D /etc/amx/wld/wld.odl
