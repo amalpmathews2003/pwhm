5a5e624a981d1478a459ba3573f3f164dbb16006
NGUYEN DINH HIEN Michael Thien Bao <michael.nguyen@sagemcom.com>
2024-10-25 16:32:59 +0200

    Issue: PPM-3034 PWHM Wi-Fi hostap updated security configuration for Wi-Fi 7
    Wi-Fi 7 introduces new mandatory security methods and features
    - AKM-24
    - GCMP-256
    - Beacon Protection
    
    In the hostapd / wpa_supplicant configuration, if the AP is linked to a
    Wi-Fi 7 operating standard radio and has a WPA3 or WPA3-Transition security
    mode enabled, then:
    - Append SAE-EXT-KEY to the wpa_key_mgmt (resp. key_mgmt in supplicant)
    - Append GCMP-256 to the wpa_pairwise (resp. pairwise in supplicant)
    - Add beacon_prot=1 in the hostapd configuration.
    
    Assumptions:
    - Any Wi-Fi 7 capable radio implements AKM-24, GCMP-256 and beacon
    protection as part of the mandatory features of Wi-Fi 7 specification.
    - AKM-24, GCMP-256 only is not expected to be configured by an end-user.
    So a AKM-8/AKM-24 & CCMP/GCMP-256 transition mode is currently always
    expected for now.
    
    Signed-off-by: NGUYEN DINH HIEN Michael Thien Bao <michael.nguyen@sagemcom.com>
    
    Closes PPM-3034

--- src/nl80211/wld_hostapd_cfgFile.c
+++ src/nl80211/wld_hostapd_cfgFile.c
@@ -866,9 +866,9 @@ static bool s_setVapCommonConfig(T_AccessPoint* pAP, swl_mapChar_t* vapConfigMap
         swl_mapCharFmt_addValStr(vapConfigMap, "wpa_key_mgmt", "%s%s%s",
                                  ((mfp != SWL_SECURITY_MFPMODE_REQUIRED) ? "WPA-PSK " : ""),
                                  ((mfp == SWL_SECURITY_MFPMODE_REQUIRED) ? "WPA-PSK-SHA256 " : ""),
-                                 "SAE");
-        swl_mapChar_add(vapConfigMap, "wpa_pairwise", "CCMP");
-        swl_mapChar_add(vapConfigMap, "rsn_pairwise", "CCMP");
+                                 (wld_rad_checkEnabledRadStd(pRad, SWL_RADSTD_BE) ? "SAE SAE-EXT-KEY" : "SAE"));
+        swl_mapChar_add(vapConfigMap, "wpa_pairwise", wld_rad_checkEnabledRadStd(pRad, SWL_RADSTD_BE) ? "CCMP GCMP-256" : "CCMP");
+        swl_mapChar_add(vapConfigMap, "rsn_pairwise", wld_rad_checkEnabledRadStd(pRad, SWL_RADSTD_BE) ? "CCMP GCMP-256" : "CCMP");
         swl_mapCharFmt_addValInt32(vapConfigMap, "wpa_group_rekey", pAP->rekeyingInterval);
         swl_mapChar_add(vapConfigMap, "wpa_ptk_rekey", "0");
         swl_mapChar_add(vapConfigMap, wpa_key_str, pAP->keyPassPhrase);
@@ -884,15 +884,18 @@ static bool s_setVapCommonConfig(T_AccessPoint* pAP, swl_mapChar_t* vapConfigMap
         swl_mapChar_add(vapConfigMap, "sae_groups", "19 20 21");
         swl_mapCharFmt_addValInt32(vapConfigMap, "ieee80211w", mfp);
         swl_mapCharFmt_addValInt32(vapConfigMap, "sae_pwe", isH2E ? 2 : 0);
+        if (wld_rad_checkEnabledRadStd(pRad, SWL_RADSTD_BE)) {
+            swl_mapChar_add(vapConfigMap, "beacon_prot", "1");
+        }
         break;
     }
     case SWL_SECURITY_APMODE_WPA3_P: {
         swl_mapChar_add(vapConfigMap, "wpa", "2");
         swl_mapCharFmt_addValStr(vapConfigMap, "wpa_key_mgmt", "%s%s",
-                                 "SAE ",
-                                 (pAP->IEEE80211rEnable ? "FT-SAE " : ""));
-        swl_mapChar_add(vapConfigMap, "wpa_pairwise", "CCMP");
-        swl_mapChar_add(vapConfigMap, "rsn_pairwise", "CCMP");
+                                 (wld_rad_checkEnabledRadStd(pRad, SWL_RADSTD_BE) ? "SAE SAE-EXT-KEY" : "SAE"),
+                                 (pAP->IEEE80211rEnable ? (wld_rad_checkEnabledRadStd(pRad, SWL_RADSTD_BE) ? " FT-SAE FT-SAE-EXT-KEY" : " FT-SAE") : ""));
+        swl_mapChar_add(vapConfigMap, "wpa_pairwise", wld_rad_checkEnabledRadStd(pRad, SWL_RADSTD_BE) ? "CCMP GCMP-256" : "CCMP");
+        swl_mapChar_add(vapConfigMap, "rsn_pairwise", wld_rad_checkEnabledRadStd(pRad, SWL_RADSTD_BE) ? "CCMP GCMP-256" : "CCMP");
         swl_mapCharFmt_addValInt32(vapConfigMap, "wpa_group_rekey", pAP->rekeyingInterval);
         swl_mapChar_add(vapConfigMap, "wpa_ptk_rekey", "0");
         // If sae_password is set, wpa_passphrase is ignored by hostapd
@@ -907,19 +910,25 @@ static bool s_setVapCommonConfig(T_AccessPoint* pAP, swl_mapChar_t* vapConfigMap
         swl_mapChar_add(vapConfigMap, "sae_groups", "19 20 21");
         swl_mapChar_add(vapConfigMap, "ieee80211w", "2");
         swl_mapCharFmt_addValInt32(vapConfigMap, "sae_pwe", isH2E ? is6g ? 1 : 2 : 0);
+        if (wld_rad_checkEnabledRadStd(pRad, SWL_RADSTD_BE)) {
+            swl_mapChar_add(vapConfigMap, "beacon_prot", "1");
+        }
         break;
     }
     case SWL_SECURITY_APMODE_OWE:
     {
         swl_mapChar_add(vapConfigMap, "wpa", "2");
-        swl_mapChar_add(vapConfigMap, "wpa_pairwise", "CCMP");
-        swl_mapChar_add(vapConfigMap, "rsn_pairwise", "CCMP");
+        swl_mapChar_add(vapConfigMap, "wpa_pairwise", wld_rad_checkEnabledRadStd(pRad, SWL_RADSTD_BE) ? "CCMP GCMP-256" : "CCMP");
+        swl_mapChar_add(vapConfigMap, "rsn_pairwise", wld_rad_checkEnabledRadStd(pRad, SWL_RADSTD_BE) ? "CCMP GCMP-256" : "CCMP");
         swl_mapChar_add(vapConfigMap, "wpa_key_mgmt", "OWE");
         swl_mapChar_add(vapConfigMap, "ieee80211w", "2");
         T_AccessPoint* transitionAp;
         if((!swl_str_isEmpty(pAP->oweTransModeIntf)) &&
            ((transitionAp = wld_ap_getVapByName(pAP->oweTransModeIntf)) != NULL)) {
             swl_mapChar_add(vapConfigMap, "owe_transition_ifname", transitionAp->alias);
+        }
+        if (wld_rad_checkEnabledRadStd(pRad, SWL_RADSTD_BE)) {
+            swl_mapChar_add(vapConfigMap, "beacon_prot", "1");
         }
         break;
     }
