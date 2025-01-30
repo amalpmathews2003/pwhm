7d96435b5a149b74c13c898cf11d35ea11aba74b
sahbot <support.opensource@softathome.com>
2025-01-30 10:08:09 +0100

    Issue: SSW-00000 - add Endpoint debug
    Feature update: add endpoint debug commands for
    wpa supplicant config file and daemon state.
    
    GitOrigin-RevId: 3435d869dc3cb1a2e113dea8aa6a043dbfce72b4

--- scripts/debugInfo.sh
+++ scripts/debugInfo.sh
@@ -27,7 +27,7 @@ for FILE in /tmp/*_hapd.conf; do
 done
 
 
-interfaces=$(iw dev | grep Interface | awk '{print $2}' | sort)
+interfaces=$(iw dev | grep -E "Interface|type" | grep -B 1 "AP" | grep Interface | awk '{print $2}'| sort)
 for INTF in $interfaces; do
   echo ""
   echo "#### hostapd state ${INTF}"
@@ -36,4 +36,25 @@ for INTF in $interfaces; do
   printCmd "$SUDO hostapd_cli -p /var/run/hostapd -i ${INTF} status"
   printCmd "$SUDO hostapd_cli -p /var/run/hostapd -i ${INTF} all_sta"
   printCmd "$SUDO hostapd_cli -p /var/run/hostapd -i ${INTF} wps_get_status"
+done
+
+
+
+for FILE in /tmp/*_wpa_supplicant.conf; do
+    echo ""
+    echo ""
+    echo "#### wpa supplicant config ${FILE}"
+    echo ""
+    cat $FILE
+done
+
+
+epEnterfaces=$(iw dev | grep -E "Interface|type" | grep -B 1 "managed" | grep Interface | awk '{print $2}'| sort)
+for INTF in $epEnterfaces; do
+  echo ""
+  echo "#### wpa_supplicant state ${INTF}"
+  printCmd "$SUDO wpa_cli -p /var/run/wpa_supplicant -i ${INTF} status"
+  printCmd "$SUDO wpa_cli -p /var/run/wpa_supplicant -i ${INTF} dump"
+  printCmd "$SUDO wpa_cli -p /var/run/wpa_supplicant -i ${INTF} list_networks"
+  printCmd "$SUDO wpa_cli -p /var/run/wpa_supplicant -i ${INTF} scan_results"
 done
