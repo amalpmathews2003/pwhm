217311d2af5250df9af9cfea1e6e9d443df683cd
sahbot <support.opensource@softathome.com>
2025-02-21 14:37:46 +0100

    Issue: PPW-404 - fix MultiAP not set for fronthaul.
    Problem: when backhauls are ordered after fronthauls in the odl, the
    configuration of the hostapd will miss the multi_ap parameters, since no
    backahul reference will be found when configuring the fronthaul.
    
    Cause: dependency backhaul/fronhaul not handled.
    
    Fix: configure all AP instances that have MultiAPType = BackhaulBSS
    save last config
    configure all AP instances that have MultiAPType & FronthaulBSS in
    second loop, if needed, use saved config
    
    (fix commit "Issue: SSW-9574 - remove reordering list of bss.")
    
    GitOrigin-RevId: 956e4c32e9c9e874925dff272a39d4ab491a991a

--- src/nl80211/wld_hostapd_cfgManager.c
+++ src/nl80211/wld_hostapd_cfgManager.c
@@ -165,19 +165,30 @@ bool wld_hostapd_createConfig(wld_hostapd_config_t** pConf, T_Radio* pRad) {
     swl_mapChar_t* radConfigMap = wld_hostapd_getConfigMap(config, NULL);
     wld_hostapd_cfgFile_setRadioConfig(pRad, radConfigMap);
     T_AccessPoint* pTmpAp = NULL;
-    swl_mapChar_t* defMultiAPConfig = NULL;
+    swl_mapChar_t* multiAPbhConfig = NULL;
+
+    // configure all pure-BH first
     amxc_llist_for_each(it, &config->vaps) {
         wld_hostapdVapInfo_t* vapInfo = NULL;
         if((vapInfo = amxc_llist_it_get_data(it, wld_hostapdVapInfo_t, it)) != NULL) {
             pTmpAp = wld_getAccesspointByAddress(vapInfo->bssid.bMac);
-            if((pTmpAp != NULL) && (wld_hostapd_ap_needWpaCtrlIface(pTmpAp))) {
-                swl_mapChar_t* multiAPConfig = defMultiAPConfig;
+            if((pTmpAp != NULL) && (wld_hostapd_ap_needWpaCtrlIface(pTmpAp)) &&
+               (SWL_BIT_IS_ONLY_SET(pTmpAp->multiAPType, MULTIAP_BACKHAUL_BSS))) {
+                multiAPbhConfig = wld_hostapd_getConfigMapByBssid(config, (swl_macBin_t*) pTmpAp->pSSID->BSSID);
+                wld_hostapd_cfgFile_setVapConfig(pTmpAp, multiAPbhConfig, NULL);
+            }
+        }
+    }
+
+    // multiAPbhConfig contains last pure-BH;
+    amxc_llist_for_each(it, &config->vaps) {
+        wld_hostapdVapInfo_t* vapInfo = NULL;
+        if((vapInfo = amxc_llist_it_get_data(it, wld_hostapdVapInfo_t, it)) != NULL) {
+            pTmpAp = wld_getAccesspointByAddress(vapInfo->bssid.bMac);
+            if((pTmpAp != NULL) && (wld_hostapd_ap_needWpaCtrlIface(pTmpAp)) &&
+               (!SWL_BIT_IS_ONLY_SET(pTmpAp->multiAPType, MULTIAP_BACKHAUL_BSS))) {
                 swl_mapChar_t* vapConfigMap = wld_hostapd_getConfigMapByBssid(config, (swl_macBin_t*) pTmpAp->pSSID->BSSID);
-                if(SWL_BIT_IS_ONLY_SET(pTmpAp->multiAPType, MULTIAP_BACKHAUL_BSS)) {
-                    defMultiAPConfig = defMultiAPConfig ? : vapConfigMap;
-                    multiAPConfig = vapConfigMap;
-                }
-                wld_hostapd_cfgFile_setVapConfig(pTmpAp, vapConfigMap, multiAPConfig);
+                wld_hostapd_cfgFile_setVapConfig(pTmpAp, vapConfigMap, multiAPbhConfig);
             }
         }
     }
