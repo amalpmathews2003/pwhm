288e8c71b801c755ab6eabb22291e80a804febf5
NGUYEN DINH HIEN Michael Thien Bao <michael.nguyen@sagemcom.com>
2025-05-13 11:33:17 +0200

    Issue: PPM-3302 OperatingChannelBandwidth in NeighboringWiFiDiagnostic is wrong if the BSS is on 320MHz
    Description:
    - In s_copyScanInfoFromIEs, fix the center channel computation by using SWL_CHANSPEC_NEW_EXT
    - In s_addDiagSingleResultToMap, rework the channel bandwidth computation based on the center channel instead of hardcoding the 320MHz set value based on the extentionHigh.

--- src/nl80211/wld_nl80211_parser.c
+++ src/nl80211/wld_nl80211_parser.c
@@ -1380,7 +1380,7 @@ static void s_copyScanInfoFromIEs(wld_scanResultSSID_t* pResult, swl_wirelessDev
         pResult->bandwidth = swl_chanspec_bwToInt(pWirelessDevIE->operChanInfo.bandwidth);
         pResult->extensionChannel = pWirelessDevIE->operChanInfo.extensionHigh;
     }
-    swl_chanspec_t chanSpec = SWL_CHANSPEC_NEW(pResult->channel, pWirelessDevIE->operChanInfo.bandwidth, pWirelessDevIE->operChanInfo.band);
+    swl_chanspec_t chanSpec = SWL_CHANSPEC_NEW_EXT(pResult->channel, pWirelessDevIE->operChanInfo.bandwidth, pWirelessDevIE->operChanInfo.band, pResult->extensionChannel, 0);
     pResult->centreChannel = swl_chanspec_getCentreChannel(&chanSpec);
     swl_operatingClass_t operClass = swl_chanspec_getOperClass(&chanSpec);
     if(operClass > 0) {
